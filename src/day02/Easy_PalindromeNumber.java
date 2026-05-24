afd9fc27ecf215a56333efbe51ad743b02e726c4
Rahul23121995 <rahulnshanbag@rediffmail.com>
2026-05-24 21:21:19 +0530

    feat(day02): add Easy solution - Easy_PalindromeNumber

---
+++ src/day02/Easy_PalindromeNumber.java
@@ -0,0 +1,14 @@
+package day02;
+public class Easy_PalindromeNumber {
+    public static boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        int reverted = 0;
+        while (x > reverted) { reverted = reverted * 10 + x % 10; x /= 10; }
+        return x == reverted || x == reverted / 10;
+    }
+    public static void main(String[] args) {
+        assert isPalindrome(121) == true;
+        assert isPalindrome(-121) == false;
+        System.out.println("Palindrome Number passed!");
+    }
+}
